ced4379841b655687c350116a3b23075d027c7b4
Landon Abney <Landonabney@gmail.com>
2016-09-28 13:45:52 -0700

    Cleanup the specs a bit
    Removes redundant tests.

--- spec/linter-reek-spec.js
+++ spec/linter-reek-spec.js
@@ -41,16 +41,10 @@ describe('The reek provider for Linter', () => {
         const messageHtml = 'IrresponsibleModule: Dirty has no descriptive comment ' +
           '[<a href=\'https://github.com/troessner/reek/blob/master/docs/Irresponsible-Module.md\'>Irresponsible-Module</a>]';
         return lint(editor).then((messages) => {
-          expect(messages[0].type).toBeDefined();
           expect(messages[0].type).toEqual('warning');
           expect(messages[0].text).not.toBeDefined();
-          expect(messages[0].html).toBeDefined();
           expect(messages[0].html).toEqual(messageHtml);
-          expect(messages[0].filePath).toBeDefined();
           expect(messages[0].filePath).toMatch(/.+bad\.rb$/);
-          expect(messages[0].range).toBeDefined();
-          expect(messages[0].range.length).toBeDefined();
-          expect(messages[0].range.length).toEqual(2);
           expect(messages[0].range).toEqual([[0, 0], [0, 0]]);
         });
       });
@@ -61,7 +55,7 @@ describe('The reek provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(goodFile).then(editor =>
         lint(editor).then(messages =>
-          expect(messages.length).toEqual(0)
+          expect(messages.length).toBe(0)
         )
       )
     );
